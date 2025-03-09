49243a8bf5f26b3a74308dabb4c83964393db06b
nairit132 <nairithmajmundar@gmail.com>
2025-03-09 17:44:58 +0000

    Add files via upload

---
+++ activity one pt2.py
@@ -0,0 +1,11 @@
+a = int(input("tye a number"))
+b = int(input("type a number"))
+c = int(input("type a number"))
+if a > 0 or b > 0 :
+    print("Either number is gerater than 0")
+else:
+    print("No nuber is greater than 0")
+if c > 0 or b > 0 :
+    print("Either number is gerater than 0")
+else:
+    print("No nuber is greater than 0")
---
+++ activity one.py
@@ -0,0 +1,7 @@
+a = int(input("Type number one"))
+b = int(input("Type number two"))
+c = int(input("Type number three"))
+if a > 0 and b > 0-45 and c > 0:
+    print("all numbers are positive")
+else:
+    print("one or more numbers are negitive")

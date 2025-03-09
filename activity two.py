e6dd7c6f690b113e05a4ac181bf754e0e8a1291a
nairit132 <nairithmajmundar@gmail.com>
2025-03-09 18:00:25 +0000

    Add files via upload

---
+++ activity two.py
@@ -0,0 +1,16 @@
+a = 32
+b = 35
+c = 65
+print(a != b)
+print(b != c)
+a = "python"
+b = "javascript"
+if a != b:
+    print("A and B are two diffrent words")
+a = 2
+b = 4
+if (a == 5) != (b == 4):
+    print("Nooooooooooo!!!!!!")
+a = int(input("number please"))
+if a%2 != 0:
+    print(a," is not a EVEN NUMBER!!!!")
